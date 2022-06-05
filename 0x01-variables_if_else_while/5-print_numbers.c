49039b9fefd9dea9a3351a89595351fc880b19c6
Amanuel <amansquad180@gmail.com>
2022-06-05 12:15:20 -0700

    If else

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) /*positive or negative*/
+{
+for (int n=0 ; n<10 ; n++)
+printf("%d", n);
+
+return (0);
+}
