44b8ef832daad58f9908a95beb79e54e56210e19
Amanuel <amansquad180@gmail.com>
2022-06-06 11:24:43 -0700

    If else

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)/*prints the alphabet in lowercase, followed by a new line.*/
+{
+int i = '0';
+while (i <= '9')
+{
+putchar(i);
+i++;
+}
+char ch = 'a';
+while (ch < 'g')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
