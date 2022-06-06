3d7b0dacbd4238feb7da5341d54fcfc9f76f04f9
Amanuel <amansquad180@gmail.com>
2022-06-06 04:07:11 -0700

    If else

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)/*prints the alphabet in lowercase, followed by a new line.*/
 {
 char ch = 'a';
