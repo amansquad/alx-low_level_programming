833ab8cecb341bc3ccaec268fa22f242695b49f4
Amanuel <amansquad180@gmail.com>
2022-06-08 10:56:57 -0700

    Nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)/*prints the alphabet in lowercase, followed by a new line.*/
-{
+int main(void){/*prints the alphabet in lowercase, followed by a new line.*/
 printf("_putchar\n");
 return (0);
 }
