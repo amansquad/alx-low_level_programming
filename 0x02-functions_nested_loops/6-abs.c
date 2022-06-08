3392248da03d45aee060be4f52904ce3f7a65aca
Amanuel <amansquad180@gmail.com>
2022-06-08 13:19:18 -0700

    Nested

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs(int) -> checks the absolute value of numbers
+ * @n: a number
+ * Return: n, 0 or -n
+ */
+int _abs(int n)
+{
+if (n > 0)
+return (n);
+
+else if (n < 0)
+
+n *= -1;
+return (n);
+
+else
+return (0);
+
+}
