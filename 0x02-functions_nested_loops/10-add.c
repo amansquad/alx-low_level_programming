19ffd0d447ddde9e93d6ec978633dfb30e8c5591
Amanuel <amansquad180@gmail.com>
2022-06-08 15:16:39 -0700

    Nested

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * add -> prints the 24 hour time
- *
+ *@a: a number
+ *@b: a number
  * Return: i and j
  */
 int add(int a, int b)
 {
-return(a + b);
+return (a + b);
 }
