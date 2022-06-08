369a0e7f655c13b25f67cd3a80211b3a2f4ae8d1
Amanuel <amansquad180@gmail.com>
2022-06-08 14:33:11 -0700

    Nested

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,12 +6,13 @@
  */
 void times_table(void)
 {
-int i, j;
-for (i = '0'; i < '10'; i++)
+int i, j, x;
+for (i = 0; i < 10; i++)
 {
-for (j = '0'; j < '10'; j++)
+for (j = 0; j < 10; j++)
 {
-_putchar(i * j);
+x = i * j;
+_putchar(x);
 if (j == 9)
 _putchar('\n');
 else
