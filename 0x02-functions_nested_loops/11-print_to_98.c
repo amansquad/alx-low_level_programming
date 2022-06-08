449c48429ecd16754a42e2b5ffbdda1fbc0c8e6e
Amanuel <amansquad180@gmail.com>
2022-06-08 15:21:10 -0700

    Nested

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_to_98 -> prints the 24 hour time
+ *@n: a number
+ * Return: i and j
+ */
+void print_to_98(int n)
+{
+while ( n <= 98)
+{
+_putchar(n);
+_putchar(',');
+}
+_putchar('\n');
+}
