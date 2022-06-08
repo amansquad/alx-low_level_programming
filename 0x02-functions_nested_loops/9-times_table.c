feb06e2fce2b5aa98cbf5a1c3ceb7ada134e70d4
Amanuel <amansquad180@gmail.com>
2022-06-08 14:27:35 -0700

    Nested

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_table -> prints the 24 hour time
+ *
+ * Return: i and j
+ */
+void times_table(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+_putchar(i * j);
+if (j = 9)
+_putchar('\n');
+else
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+
+}
