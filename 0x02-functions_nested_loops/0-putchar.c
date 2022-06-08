1476f44153e7d2c11c96c14d8b5045440a61a61c
Amanuel <amansquad180@gmail.com>
2022-06-08 10:19:56 -0700

    Nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)/*prints the alphabet in lowercase, followed by a new line.*/
+{
+printf("_putchar\n");
+return (0);
+}
