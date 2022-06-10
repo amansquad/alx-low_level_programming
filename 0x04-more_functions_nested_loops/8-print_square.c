1152d7a49534d2abcbc5867c217422d239318354
Amanuel <amansquad180@gmail.com>
2022-06-11 00:30:12 +0300

    Functions

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_square -> print numbers
+* @size: an integer
+*/
+void print_square(int size)
+{
+int x, i;
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (x = 0; x < size; x++)
+{
+for (i = 0; i < size; i++)
+_putchar(35);
+}
+_putchar(35);
+}
+}
