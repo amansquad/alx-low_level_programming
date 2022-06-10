b90b6a91500fe1154b7b3d2f4c1850c710b924d0
Amanuel <amansquad180@gmail.com>
2022-06-10 23:34:59 +0300

    Functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_numbers -> this is a function to check upper case of alphabet
+* Return: something but success
+*/
+void print_numbers(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+_putchar(x + '0');
+_putchar('\n');
+}
+}
