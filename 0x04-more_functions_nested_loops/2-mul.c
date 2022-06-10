fd8b3bb99e0d5bf1f297a0b80e64f7ec0a25960e
Amanuel <amansquad180@gmail.com>
2022-06-10 23:31:06 +0300

    Functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul -> this is a function to check upper case of alphabet
+* @a: an integer parameter
+* @b: an integer parameter
+* Return: something but success
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
