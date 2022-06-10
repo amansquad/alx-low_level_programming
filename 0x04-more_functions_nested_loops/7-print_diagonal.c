52ac9e387d35cdd9939e40379c972faadd9ab103
Amanuel <amansquad180@gmail.com>
2022-06-11 00:19:04 +0300

    Functions

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_diagonal -> print numbers
+* @n: an integer
+*/
+void print_diagonal(int n)
+{
+int x;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (x = 0; x < n; x++)
+_putchar('\');
+_putchar('\n');
+}
+}
